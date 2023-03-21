382f4062ec5051bd9aaa039792f62fdce240db15
Pavlo Veremeienko <pavel.veremeienko@gmail.com>
2023-03-21 16:31:10 +0200

    Add calculator class

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static double GetResult(double operand1, double operand2, Operation operation)
+        {
+            double result = double.NaN;
+            switch (operation)
+            {
+                case Operation.Add:
+                    result = operand1 + operand2;
+                    break;
+                case Operation.Subtract:
+                    result = operand1 - operand2;
+                    break;
+                case Operation.Multiply:
+                    result = operand1 * operand2;
+                    break;
+                case Operation.Divide:
+                    result = operand1 / operand2;
+                    break;
+            }
+            return result;
+        }
+    }
+
+    public enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+}
